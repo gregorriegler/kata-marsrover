e49d64d3c7687385478f3d95a217cff3d02c990f
Gregor Riegler <gregor.riegler@ebcont.com>
2019-04-07 13:00:43 +0200

    refactor move

--- src/test/java/Rover.java
+++ src/test/java/Rover.java
@@ -20,7 +20,7 @@ public class Rover {
     }
 
     public void move(String move) {
-        this.position = Move.of(move).move(this.direction, this.position);
+        this.position = Move.of(move).move(this.position, this.direction);
     }
 
     public void turn(String turn) {
@@ -40,12 +40,12 @@ public class Rover {
             return new Position(x, y);
         }
 
-        private Position goHorizontal(int step) {
-            return of(x + step, y);
+        public Position addToX(int newX) {
+            return of(x + newX, y);
         }
 
-        private Position goVertical(int step) {
-            return of(x, y + step);
+        public Position addToY(int newY) {
+            return of(x, y + newY);
         }
 
         @Override
@@ -69,7 +69,7 @@ public class Rover {
     }
 
     public enum Direction {
-        N, W, S, E;
+        N(1), W(-1), S(-1), E(+1);
 
         static {
             N.rightOf = E;
@@ -82,10 +82,15 @@ public class Rover {
             E.leftOf = N;
         }
 
+        private final int vector;
         private Direction rightOf;
         private Direction leftOf;
 
         public static final String TURN_R = "r";
+
+        Direction(int vector) {
+            this.vector = vector;
+        }
 
         private Direction turn(String turn) {
             if (TURN_R.equals(turn)) {
@@ -102,11 +107,11 @@ public class Rover {
         BACKWARD("b", -1);
 
         private final String asString;
-        private final int step;
+        private final int vector;
 
-        Move(String asString, int step) {
+        Move(String asString, int vector) {
             this.asString = asString;
-            this.step = step;
+            this.vector = vector;
         }
 
         public static Move of(String value) {
@@ -117,16 +122,14 @@ public class Rover {
             return move;
         }
 
-        private Position move(Direction direction, Position position) {
-            if(Direction.W.equals(direction)) {
-                return position.goHorizontal(step * -1);
-            } else if (Direction.E.equals(direction)) {
-                return position.goHorizontal(step * 1);
-            } else if (Direction.S.equals(direction)) {
-                return position.goVertical(step * -1);
+        private Position move(Position position, Direction direction) {
+            int vector = this.vector * direction.vector;
+            if (Direction.W.equals(direction) || Direction.E.equals(direction)) {
+                return position.addToX(vector);
             } else {
-                return position.goVertical(step * 1);
+                return position.addToY(vector);
             }
         }
+
     }
 }
